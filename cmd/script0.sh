54c28aea9831dc789d9e8e0a58cb71c77e859fc6
andrespalechor <andrespalechor11@gmail.com>
2022-11-04 15:11:09 +0100

    removing scripts

--- cmd/script0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 nice -n 19 python3 src/train.py exp_name=b1n_l1_e0_adam train_mode=train data.train_file=v1/p1_train.csv data.val_file=v1/p1_val.csv data.test_file=v1/p1_test.csv loss.type=BGsoftmax opt.type=adam opt.lr=0.001 batch_size=64 epochs=120 workers=4 seed=42
-
-
-
-
-
